07a028c5f8009cee69b62e9f2766b7deda40622a
Zzceaon <zzceaon@outlook.com>
2020-05-14 02:16:14 +0800

    modify src/go_code/project01/main/hello.go

--- src/go_code/project01/main/hello.go
+++ src/go_code/project01/main/hello.go
@@ -1,7 +1,3 @@
-// 可go run 和 go build(实际生产中用go build再运行exe文件)
-// exe文件在没有go的环境下任然可以运行,因此文件变大了(打包了go环境)
-// go run需要go运行环境
-// go build 可指定生成的文件名:go build -o myhello.exe hello.go
 package main
 import "fmt"
 func main()  {
@@ -10,4 +6,3 @@ func main()  {
 	fmt.Println("hello world!")
 	fmt.Println("hello world!")
 }
-// 重命名 go build -o myhello.exe hello.go
