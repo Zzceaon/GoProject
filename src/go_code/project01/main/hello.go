9e9630bcfdabb90c6f7b8e37b912b14a86324bed
Zzceaon <zzceaon@outlook.com>
2020-05-16 01:37:36 +0800

    modify project01/main/hello.go

--- src/go_code/project01/main/hello.go
+++ src/go_code/project01/main/hello.go
@@ -1,8 +1,19 @@
 package main
+
 import "fmt"
-func main()  {
+
+func main() {
 	// 快速复制:shift + alt + 上/下
 	fmt.Println("hello world!")
 	fmt.Println("hello world!")
 	fmt.Println("hello world!")
 }
+
+// 错误风格
+// func main() 
+// {
+// 	// 快速复制:shift + alt + 上/下
+// 	fmt.Println("hello world!")
+// 	fmt.Println("hello world!")
+// 	fmt.Println("hello world!")
+// }
